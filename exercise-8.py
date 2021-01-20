29cf2533bc271c7142ebae2cc70ee7211d2f9689
Eric Mathias <emathias@gmail.com>
2021-01-19 19:37:57 -0500

    finishing touches

--- exercise-8.py
+++ exercise-8.py
@@ -1,2 +1,6 @@
 # Using the tuple `foods` and list comprehension within a `for` loop, print each food string that contains the letter `a`.
 foods = ('apple', 'banana', 'avocado', 'pear', 'peach')
+
+for food in foods:
+    if "a" in food:
+        print(food)
